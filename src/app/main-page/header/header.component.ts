4a162905275338d4bc82a6c31f3d2cc3a61d65d6
Daniela Gonzalez <danygogo@gmail.com>
2022-01-14 09:14:16 -0600

    changes before adding guards

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+  name: string = ""
+  uid: string =""
 
   ngOnInit(): void {
+    this.uid = this.authService.userID
+    console.log(this.uid)
+
+    if(this.uid){
+      this.name = sessionStorage.getItem(this.uid)!
+    }
+
   }
 
 }
